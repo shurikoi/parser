e81326954fe7295246eb0c4f189c6f04ead0db30
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2023-07-08 10:22:53 +0200

    Some changes

--- script.js
+++ script.js
@@ -6,20 +6,27 @@ console.log('Start')
 
 // Fetches data from a website
 axios('https://nationaltoday.com/what-is-today/')
-    .then(response => {
+.then(response => {
         const $ = cheerio.load(response.data)
         let output = ''
+        console.log('Data processing...')
 
-        // Iterates one by one element with class 'holiday-title'
-        $('.holiday-title').each((i, el) => {
-            output += `${$(el).text()}\n`
+        return new Promise((resolve, reject) => {
+            // Iterates one by one element with class 'holiday-title'
+            $('.holiday-title').each((i, el) => {
+                output += `${$(el).text()}\n`
+            })
+            fs.writeFile('output.txt', output, error => {
+                if (error) {
+                    console.error(error)
+                    return
+                }
+            })
+            resolve()
         })
-        fs.writeFile('output.txt', output, error => {
-            if (error) {
-                console.error(error)
-                return
-            }
-        })
-        console.log('Success!')
+    })
+    .then(() => {
+        console.log('Success! Check the "txt" file')
     })
     .catch(error => console.error(error.message))
+    
